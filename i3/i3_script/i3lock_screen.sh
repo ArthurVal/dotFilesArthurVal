93f07debae47a09b5af464284bbc5c8c0c0df288
Arthur Valiente <arthur.valiente@esrf.fr>
2021-09-09 08:26:18 +0200

    Add a test to not take the screenshot when the file already exists

--- i3/i3_script/i3lock_screen.sh
+++ i3/i3_script/i3lock_screen.sh
@@ -4,11 +4,14 @@
 # icon="$HOME/images/lock-icon-light.png"
 tmpbg='/tmp/lock_screen.png'
 
-# take a screenshot
-scrot "$tmpbg"
 
-# blur the screenshot by resizing and scaling back up
-convert "$tmpbg" -filter Gaussian -thumbnail 20% -sample 500% "$tmpbg"
+if [ ! -f "$tmpbg" ]; then
+    # take a screenshot
+    scrot  "$tmpbg"
+
+    # blur the screenshot by resizing and scaling back up
+    convert "$tmpbg" -filter Gaussian -thumbnail 20% -sample 500% "$tmpbg"
+fi
 
 # overlay the icon onto the screenshot
 # convert "$tmpbg" "$icon" -gravity center -composite "$tmpbg"
